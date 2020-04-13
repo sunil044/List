64ffb6f7f9c3e34ac6d009a4d538b4311de5843e
sunil044 <63278743+sunil044@users.noreply.github.com>
2020-04-13 18:29:32 +0530

    Add files via upload

---
+++ Arrays2.java
@@ -0,0 +1,22 @@
+package arrays;
+
+public class Arrays2 {
+	
+	public static void main(String[] args) {
+		
+		int [] arr1 = new int [10];
+		
+		arr1[0] = 20;
+		arr1[1] = 40;
+		arr1[2] = 60;
+		arr1[3] = 80;
+		arr1[4] = 100;
+		arr1[5] = 120;
+		
+		for(int i =0; i <= arr1.length-1; i++) {
+			System.out.println(arr1[i]);
+		}
+		
+	}
+
+}
